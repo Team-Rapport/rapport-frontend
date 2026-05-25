7706005ec236cb8d95f4698cd8f82c664c5525c3
Seoyoung Lee <lsyamyseoyoung3040@gmail.com>
2026-05-25 19:21:48 +0900

    chore: dev용 auth 토글 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from '@/router'
-import { DevAuthToggle } from '@/components/dev/DevAuthToggle'
 
 export default function App() {
   return (
     <>
       <RouterProvider router={router} />
-      <DevAuthToggle />
     </>
   )
 }
